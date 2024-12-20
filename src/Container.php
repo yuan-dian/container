b781c094486d9e5ea1458981d0d38c4cf8ecc239
原点 <467490186@qq.com>
2024-12-20 14:06:45 +0800

    feat：增加bind操作

--- src/Container.php
+++ src/Container.php
@@ -18,12 +18,14 @@ use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 use ReflectionClass;
 use ReflectionException;
+use ReflectionFunction;
 use ReflectionFunctionAbstract;
 use ReflectionNamedType;
 use ReflectionParameter;
 use yuandian\Container\Attributes\Inject;
 use yuandian\Container\Attributes\RequestScoped;
 use yuandian\Container\Exception\ClassNotFoundException;
+use yuandian\Container\Exception\FuncNotFoundException;
 
 /**
  * 容器对象
@@ -37,6 +39,12 @@ class Container implements ContainerInterface
      * @var array
      */
     private array $reflectionCache = [];
+
+    /**
+     * 容器绑定标识
+     * @var array
+     */
+    protected array $bind = [];
 
     /**
      * 容器对象实例
@@ -71,7 +79,7 @@ class Container implements ContainerInterface
     /**
      * 设置当前容器的实例
      * @access public
-     * @param object|Closure $instance
+     * @param object $instance
      * @return void
      */
     public static function setInstance($instance): void
@@ -80,17 +88,110 @@ class Container implements ContainerInterface
     }
 
     /**
+     * 根据别名获取真实类名
+     * @param string $abstract
+     * @return string
+     */
+    public function getAlias(string $abstract): string
+    {
+        if (isset($this->bind[$abstract])) {
+            return $this->bind[$abstract];
+        }
+
+        return $abstract;
+    }
+
+    /**
+     * 绑定一个类实例到全局容器
+     * @access public
+     * @param string $abstract 类名或者标识
+     * @param object $instance 类的实例
+     * @return $this
+     */
+    public function instanceGlobal(string $abstract, object $instance): static
+    {
+        $abstract = $this->getAlias($abstract);
+
+        $this->lifecycleManager->setGlobal($abstract, $instance);
+
+        return $this;
+    }
+
+    /**
+     * 绑定一个类实例到请求容器
+     * @param string $abstract
+     * @param object $instance
+     * @return $this
+     * @date 2024/12/20 13:47
+     * @author 原点 467490186@qq.com
+     */
+    public function instanceRequest(string $abstract, object $instance): static
+    {
+        $abstract = $this->getAlias($abstract);
+
+        $this->lifecycleManager->setRequest($abstract, $instance);
+
+        return $this;
+    }
+
+
+    /**
+     * 绑定一个类、闭包、实例、接口实现到容器
+     * @access public
+     * @param string|array $abstract 类标识、接口
+     * @param mixed $concrete 要绑定的类、闭包或者实例
+     * @return $this
+     */
+    public function bind(string|array $abstract, mixed $concrete = null): static
+    {
+        if (is_array($abstract)) {
+            foreach ($abstract as $key => $val) {
+                $this->bind($key, $val);
+            }
+            return $this;
+        }
+
+        // 判断是否是绑定闭包
+        if ($concrete instanceof Closure) {
+            $this->bind[$abstract] = $concrete;
+            return $this;
+        }
+
+        // 判断是否是绑定对象
+        if (is_object($concrete)) {
+            $className = get_class($concrete);
+            // 判断生命周期类型（缓存判断结果）
+            $isRequestScope = $this->lifecycleManager->getCachedLifecycle($className);
+            if ($isRequestScope === null) {
+                $isRequestScope = $this->isRequestScope($className);
+                $this->lifecycleManager->cacheLifecycle($className, $isRequestScope);
+            }
+            if ($isRequestScope && $this->lifecycleManager->isRequestCoroutine()) {
+                $this->instanceRequest($abstract, $concrete);
+            } else {
+                $this->instanceGlobal($abstract, $concrete);
+            }
+            return $this;
+        }
+
+        $abstract = $this->getAlias($abstract);
+        if ($abstract != $concrete) {
+            $this->bind[$abstract] = $concrete;
+        }
+        return $this;
+    }
+
+
+    /**
      * 获取对象的反射信息
      *
-     * @param string|object $object
+     * @param string $className
      * @return ReflectionClass
      * @date 2024/12/20 09:48
      * @author 原点 467490186@qq.com
      */
-    private function getReflectionClass(string|object $object): ReflectionClass
+    private function getReflectionClass(string $className): ReflectionClass
     {
-        $className = is_object($object) ? get_class($object) : $object;
-
         if (!isset($this->reflectionCache[$className])) {
             try {
                 $this->reflectionCache[$className] = new ReflectionClass($className);
@@ -112,9 +213,8 @@ class Container implements ContainerInterface
      */
     public function make(string $className, array $vars = [], bool $newInstance = false): ?object
     {
-        if ($newInstance) {
-            return $this->invokeClass($className, $vars);
-        }
+        $className = $this->getAlias($className);
+
         // 1. 检查全局容器是否存在实例
         $globalInstance = $this->lifecycleManager->getGlobal($className);
         if ($globalInstance !== null) {
@@ -132,14 +232,14 @@ class Container implements ContainerInterface
         if ($isRequestScope && $this->lifecycleManager->isRequestCoroutine()) {
             $requestInstance = $this->lifecycleManager->getRequest($className);
             if ($requestInstance === null) {
-                $requestInstance = $this->invokeClass($className, $vars); // 自动创建
+                $requestInstance = $this->invoke($className, $vars); // 自动创建
                 $this->lifecycleManager->setRequest($className, $requestInstance);
             }
             return $requestInstance;
         }
 
         // 4. 否则，创建并存储在全局容器
-        $instance = $this->invokeClass($className, $vars);
+        $instance = $this->invoke($className, $vars);
         $this->lifecycleManager->setGlobal($className, $instance);
         // 删除全局对象反射缓存，后续不需要重新创建对象
         unset($this->reflectionCache[$className]);
@@ -149,14 +249,14 @@ class Container implements ContainerInterface
     /**
      * 调用反射执行类的实例化 支持依赖注入
      * @access public
-     * @param string $class 类名
+     * @param string $className 类名
      * @param array $vars 参数
      * @return object|null
      * @throws ClassNotFoundException|ReflectionException
      */
-    public function invokeClass(string $class, array $vars = []): ?object
+    public function invokeClass(string $className, array $vars = []): ?object
     {
-        $classReflector = $this->getReflectionClass($class);
+        $classReflector = $this->getReflectionClass($className);
 
         if ($classReflector->hasMethod('__make')) {
             $method = $classReflector->getMethod('__make');
@@ -179,6 +279,43 @@ class Container implements ContainerInterface
             }
         }
         return $instance;
+    }
+
+    /**
+     * 执行函数或者闭包方法 支持参数调用
+     * @access public
+     * @param string|Closure $function 函数或者闭包
+     * @param array $vars 参数
+     * @return mixed
+     * @throws ReflectionException|FuncNotFoundException
+     */
+    public function invokeFunction(string|Closure $function, array $vars = []): mixed
+    {
+        try {
+            $reflect = new ReflectionFunction($function);
+        } catch (ReflectionException $e) {
+            throw new FuncNotFoundException("function not exists: {$function}()", $e);
+        }
+
+        $args = $this->bindParams($reflect, $vars);
+
+        return $function(...$args);
+    }
+
+    /**
+     * @param string|Closure $abstract
+     * @param array $vars
+     * @return mixed|object|null
+     * @throws ReflectionException
+     * @date 2024/12/20 11:34
+     * @author 原点 467490186@qq.com
+     */
+    public function invoke(string|Closure $abstract, array $vars = []): mixed
+    {
+        if ($abstract instanceof Closure) {
+            return $this->invokeFunction($abstract, $vars);
+        }
+        return $this->invokeClass($abstract, $vars);
     }
 
     /**
@@ -272,22 +409,29 @@ class Container implements ContainerInterface
      */
     public function get(string $id): ?object
     {
-        if ($this->has($id)) {
-            return $this->make($id);
+        $className = $this->getAlias($id);
+        if ($this->lifecycleManager->getGlobal($className) !== null) {
+            return $this->lifecycleManager->getGlobal($className);
+        }
+        if ($this->lifecycleManager->getRequest($className) !== null) {
+            return $this->lifecycleManager->getRequest($className);
         }
         throw new ClassNotFoundException('class not exists: ' . $id);
     }
 
     /**
      * 判断类是否为短生命周期
-     * @param string $className
+     * @param string|Closure $className
      * @return bool
      * @throws ClassNotFoundException
      * @date 2024/12/18 14:07
      * @author 原点 467490186@qq.com
      */
-    private function isRequestScope(string $className): bool
+    private function isRequestScope(string|Closure $className): bool
     {
+        if ($className instanceof Closure) {
+            return $this->lifecycleManager->isRequestCoroutine();
+        }
         $classReflector = $this->getReflectionClass($className);
 
         $attributes = $classReflector->getAttributes(RequestScoped::class);
@@ -303,6 +447,8 @@ class Container implements ContainerInterface
      */
     public function has(string $id): bool
     {
-        return $this->lifecycleManager->getGlobal($id) !== null || $this->lifecycleManager->getRequest($id) !== null;
+        $className = $this->getAlias($id);
+        return $this->lifecycleManager->getGlobal($className) !== null
+            || $this->lifecycleManager->getRequest($className) !== null;
     }
 }
